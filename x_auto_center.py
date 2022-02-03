be08c51f0f9dada8eadf03b76330edf5794e3ff9
ethanmac <ethanmac@mac.com>
2022-02-04 01:22:40 +0800

    update

--- x_auto_center.py
+++ x_auto_center.py
@@ -11,17 +11,13 @@ class dotdict(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-
-def find_img_thresh(img):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    img_blur = cv2.GaussianBlur(img_gray, (5, 5), cv2.BORDER_DEFAULT)
+def find_img_balanced(img_gray):
     #img_blur = cv2.GaussianBlur(img_blur, (5, 5), cv2.BORDER_DEFAULT)
     #img_blur = cv2.GaussianBlur(img_blur, (5, 5), cv2.BORDER_DEFAULT)
     #blur = cv2.GaussianBlur(img_gray, (16, 16), cv2.BORDER_DEFAULT)
 
-    img_dst = np.zeros_like(img_blur)
-    cv2.normalize(img_blur, img_dst, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U) 
+    img_dst = np.zeros_like(img_gray)
+    cv2.normalize(img_gray, img_dst, 0, 255, cv2.NORM_MINMAX, cv2.CV_8U) 
     img_gray_balanced = img_dst
 
     canny_low = 150
@@ -37,24 +33,42 @@ def find_img_thresh(img):
     th, img_thresh = cv2.threshold(img_gray_balanced_canny, 200, 255, cv2.THRESH_BINARY_INV)
     return img_thresh
 
-def find_cnts_img_thresh(img_thresh):
-    cnts, hierarchy = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    return cnts
+def find_img_thresh(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), cv2.BORDER_DEFAULT)
+
+    thv = 250
+    found_right_cnt = False
+    for thv in range(254, 200, -2):
+        th, img_thresh = cv2.threshold(img_blur, thv, 255, cv2.THRESH_BINARY_INV)
+        cnts, hierarchy = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in cnts:
+            if len(cnt) > 4:
+                found_right_cnt = True
+                break
+        if found_right_cnt:
+            break
+
+    return img_thresh, cnts
 
 def get_center_of_contour(cnt):
-    M = cv2.moments(cnt)
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    return cX, cY
+    try:
+        M = cv2.moments(cnt)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        return cX, cY
+    except:
+        return 0, 0
 
 def find_img_center(img):
     ix, iy = img.shape[1], img.shape[0]
-    img_thresh = find_img_thresh(img)
-    cnts = find_cnts_img_thresh(img_thresh)
+    img_thresh, cnts = find_img_thresh(img)
     print("num of cnts", len(cnts))
     if len(cnts) == 1:
         cX, cY = get_center_of_contour(cnts[0])
-        return (cX, cY)
+        return (cX, cY), 1
     elif len(cnts) > 1:
         cts = []
         cix, ciy = ix // 2,  iy // 2
@@ -64,9 +78,9 @@ def find_img_center(img):
             if (cX - cix) ** 2 + (cY - ciy) ** 2 < (cx - cix) ** 2 + (cy - ciy) ** 2:
                 cx, cy = cX, cY
             cts.append((cX, cY))
-        return cx, cy
+        return (cx, cy), len(cnts)
     else:
-        return (0, 0)
+        return (0, 0), 0
 
 def find_center_box(img, cx, cy):
     ix, iy = img.shape[1], img.shape[0]
@@ -80,9 +94,9 @@ def find_center_box(img, cx, cy):
 
     return pt0, pt1
 
-def mark_center(img, cx, cy):
+def mark_center_debug(img, cx, cy):
     img_draw = img.copy()
-    img_draw = find_img_thresh(img_draw)
+    #img_draw, _ = find_img_thresh(img_draw)
 
     pt0, pt1 = find_center_box(img, cx, cy)
 
@@ -95,12 +109,13 @@ def mark_center(img, cx, cy):
 
 def center_img(filename, args):
     img_org = cv2.imread(filename, cv2.IMREAD_COLOR)
-    cx, cy = find_img_center(img_org)
+    (cx, cy), counts = find_img_center(img_org)
 
-    if cx != 0 and cy != 0:
+    if cx != 0 and cy != 0 and counts > 0:
         print("HW_img", img_org.shape[1], img_org.shape[0])
         print("CT_img", cx, cy)
-        mark_center(img_org, cx, cy)
+        print("cn_img", counts)
+        mark_center_debug(img_org, cx, cy)
     else:
         print("skip as the image has more than one contours")
 
@@ -112,13 +127,23 @@ if __name__ == "__main__":
 
     #filename = "few-shot-plants/planets/img/S82_05.jpg"
     #filename = "few-shot-plants/planets/img/S82_03.jpg"
+    
     #filename = "few-shot-plants/planets/img/S81_05.jpg"
+    
     #filename = "few-shot-plants/planets/img/S75_11.jpg"
-    filename = "few-shot-plants/planets/img/S71_26.jpg"
-    filename = "few-shot-plants/planets/img/S01_09.jpg"
-    filename = "few-shot-plants/planets/img/S01_29.jpg"
-    filename = "few-shot-plants/planets/img/S03_13.jpg"
-    filename = "few-shot-plants/planets/img/S05_02.jpg"
+    #filename = "few-shot-plants/planets/img/S71_26.jpg"
+    #filename = "few-shot-plants/planets/img/S01_09.jpg"
+    #filename = "few-shot-plants/planets/img/S01_29.jpg"
+    
+    #filename = "few-shot-plants/planets/img/S03_13.jpg"
+    
+    #filename = "few-shot-plants/planets/img/S05_02.jpg"
     filename = "few-shot-plants/planets/img/S05_12.jpg"
+
+    filename = "few-shot-plants/planets/img/S01_04.jpg"
+    filename = "few-shot-plants/planets/img/S01_08.jpg"
+    filename = "few-shot-plants/planets/img/S01_14.jpg"
+    filename = "few-shot-plants/planets/img/S01_18.jpg"
+    filename = "few-shot-plants/planets/img/S01_22.jpg"
     center_img(filename, args)
     pass
