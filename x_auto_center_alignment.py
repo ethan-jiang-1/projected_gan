13080041514d4b6b2972f6777164e7dd13c4e465
ethanmac <ethanmac@mac.com>
2022-02-04 10:12:15 +0800

    update

--- x_auto_center_alignment.py
+++ x_auto_center_alignment.py
@@ -1,6 +1,6 @@
 import cv2 
-#import os
-#import matplotlib.pyplot as plt
+import os
+import matplotlib.pyplot as plt
 import numpy as np
 #import pprint
 #import json
@@ -94,36 +94,67 @@ def find_center_box(img, cx, cy):
 
     return pt0, pt1
 
-def mark_center_debug(img, cx, cy):
-    img_draw = img.copy()
-    #img_draw, _ = find_img_thresh(img_draw)
-
-    pt0, pt1 = find_center_box(img, cx, cy)
-
+def mark_center_debug(img_org, img_new, cx, cy, pt0, pt1):
+    img_draw = img_org.copy()
     cv2.circle(img_draw, (cx, cy), 5, (0, 0, 0), -1)
     cv2.rectangle(img_draw, pt0, pt1, (0, 0, 255), 1)
 
-    cv2.imshow("center img", img_draw)
-    cv2.waitKey(0)
+    img_thread, _ = find_img_thresh(img_org.copy())
+
+    plt.figure()
+    plt.subplot(141)
+    plt.imshow(img_org)
+    plt.subplot(142)
+    plt.imshow(img_thread)
+    plt.subplot(143)
+    plt.imshow(img_draw)
+    plt.subplot(144)
+    plt.imshow(img_new)
+    #plt.show()
+    print("Pause to showup plt.imshow")
+    plt.close()
 
 
-def center_img(filename, args):
+def gen_aligned_img(img_org, pt0, pt1,  cx, cy, filename, args):
+
+    nh = pt1[1] - pt0[1]
+    nw = pt1[0] - pt0[0]
+    img_new = np.zeros((nh, nw, img_org.shape[2]), dtype=img_org.dtype)
+    #img_new = img_org[pt0[0]:pt1[0], pt0[1]:pt1[1]]
+    img_new = img_org[pt0[1]:pt1[1], pt0[0]:pt1[0]]
+    
+    bname = os.path.basename(filename)
+    pname = "{}/{}_{}x{}.jpg".format(args.dir_dst, bname.split(".")[0], nh, nw)
+
+    if args.debug:
+        mark_center_debug(img_org, img_new, cx, cy, pt0, pt1)
+
+    cv2.imwrite(pname, img_new) 
+    return pname
+    
+
+def center_img_and_align(filename, args):
     img_org = cv2.imread(filename, cv2.IMREAD_COLOR)
     (cx, cy), counts = find_img_center(img_org)
 
     if cx != 0 and cy != 0 and counts > 0:
-        print("HW_img", img_org.shape[1], img_org.shape[0])
-        print("CT_img", cx, cy)
-        print("cn_img", counts)
-        mark_center_debug(img_org, cx, cy)
+        if args.debug:
+            print("HW_img", img_org.shape[1], img_org.shape[0])
+            print("CT_img", cx, cy)
+            print("cn_img", counts)
+
+        pt0, pt1 = find_center_box(img_org, cx, cy)
+
+        return gen_aligned_img(img_org, pt0, pt1, cx, cy, filename, args)
+
     else:
         print("skip as the image has more than one contours")
+        return None
 
-
-if __name__ == "__main__":
-
-    args = dotdict({"dir_dst": "few-shot-plants/planets-128/img",
-                    "size": 128})
+def do_exam_file():
+    args = dotdict({"dir_dst": "few-shot-plants/planets-aligned",
+                   "debug": True})
+    os.makedirs(args.dir_dst, exist_ok=True)
 
     #filename = "few-shot-plants/planets/img/S82_05.jpg"
     #filename = "few-shot-plants/planets/img/S82_03.jpg"
@@ -138,12 +169,42 @@ if __name__ == "__main__":
     #filename = "few-shot-plants/planets/img/S03_13.jpg"
     
     #filename = "few-shot-plants/planets/img/S05_02.jpg"
-    filename = "few-shot-plants/planets/img/S05_12.jpg"
+    #filename = "few-shot-plants/planets/img/S05_12.jpg"
 
     filename = "few-shot-plants/planets/img/S01_04.jpg"
     filename = "few-shot-plants/planets/img/S01_08.jpg"
     filename = "few-shot-plants/planets/img/S01_14.jpg"
     filename = "few-shot-plants/planets/img/S01_18.jpg"
     filename = "few-shot-plants/planets/img/S01_22.jpg"
-    center_img(filename, args)
-    pass
+
+    filename = "few-shot-plants/planets/img/S03_00.jpg"
+    
+    args.debug = True
+    dst_path = center_img_and_align(filename, args)
+    print(dst_path)
+
+def do_exam_folder():
+    args = dotdict({"dir_dst": "few-shot-plants/planets-aligned",
+                    "debug": False})
+    os.makedirs(args.dir_dst, exist_ok=True)
+
+    dir_src = "few-shot-plants/planets/img"
+    filenames = sorted(os.listdir(dir_src))
+    filed_filenames = []
+    for name in filenames:
+        if not name.endswith(".jpg"):
+            continue
+        filename = "{}/{}".format(dir_src, name)
+        dst_path = center_img_and_align(filename, args)
+        if dst_path is not None:
+            print(dst_path)
+        else:
+            filed_filenames.append(filename)
+
+
+if __name__ == "__main__":
+    #do_exam_file()
+    do_exam_folder()
+
+
+    
